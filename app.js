43920f4753a5fcdac16f21e6c24011180b65c36a
syukur91 <syukur@hooq.tv>
2019-11-02 15:35:16 +0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-var env = require('node-env-file');
+const env = require('node-env-file');
+const compareJSON = require('JSON-structure-validator');
 env('.env');
 
-var obj = {someAttribute: true};
-
-
-
-var firebase = require('firebase');
+const firebase = require('firebase');
 const firebaseConfig = {
     apiKey: process.env.API_KEY,
     authDomain:  process.env.AUTH_DOMAIN,
@@ -21,19 +18,23 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-var location = firebase.database().ref('/location');
-var order = firebase.database().ref('/order');
+const tracking = firebase.database().ref('/tracking');
+const order = firebase.database().ref('/order');
 
-var locationData = {
-    orderID     : "12313ADASD",
-    latitude    : "913123",
-    longitude   : "54521",
+var orderSchema = {
+    "orderID"   : "",
+    "orderName" : ""
 };
 
+var trackingSchema = {
+    "orderID"   : "",
+    "latitude"  : "",
+    "longitude" : ""
+};
 
-var orderData = {
-    orderID     : "12313ADASD",
-    orderName   : "test",
+var vehicleSchema = {
+    "vehicleID"      : "",
+    "vehicleNumber"  : ""
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -43,15 +44,43 @@ app.get('/', function (req, res) {
  return res.send('Hello world');
 });
 
-app.post('/location', function (req, res) {
-    location.push(locationData);
-    return res.send(locationData);
+
+// Add tracking data
+app.post('/tracking', function (req, res) {
+    try {
+        JSON.parse(req.body);
+        if(compareJSON(trackingSchema, req.body)) {
+            // tracking.push(locationData);
+            return res.send(req.body);
+        } else {
+            throw new Error("Format data is wrong");
+        } 
+    } catch (e) {
+        return res.send(e);
+    }
 });
 
+// Add order data
 app.post('/order', function (req, res) {
-    order.push(orderData);
-    return res.send(orderData);
+    try {
+        JSON.parse(req.body);
+        order.push(locationData);
+        return res.send(req.body);
+    } catch (e) {
+        return res.send("not a JSON");
+    }
+});
+
+
+// Add fleet data
+app.post('/fleet', function (req, res) {
+    try {
+        JSON.parse(req.body);
+        return res.send(req.body);
+    } catch (e) {
+        return res.send("not a JSON");
+    }
 });
 
 app.listen(process.env.PORT || 8080);
-console.log('Loocads API run on port ' + process.env.PORT);
+console.log('Tracking API run on port ' + process.env.PORT);
